78a2676d9b97f886e2261b78ed92fb0f2cccad65
cristianmoreno430 <55451316+cristianmoreno430@users.noreply.github.com>
2024-08-22 17:57:41 -0500

    Check in proyecto organizado

---
+++ TestTaskManage/TaskServicePerformanceTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Diagnostics;
+using TaskBussines;
+using TaskBussines.Interfaces;
+using TaskData;
+using TaskData.Interfaces;
+
+namespace TestTaskManage
+{
+    public class TaskServicePerformanceTest
+    {
+        private readonly ITasksService _service;
+        private readonly ITaskInformation _repository; // Dependencia real  
+        public TaskServicePerformanceTest()
+        {
+            _repository = new TaskInformation(); // Usando una implementación real
+            _service = new TasksService(_repository);
+        }
+
+        [Fact]
+        public void CrearTareas_DeberiaSerRapido()
+        {
+            // Arrange            
+
+            // Act 
+            var stopwatch = Stopwatch.StartNew();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                _service.CrearTarea($"Tarea {i}");
+            }
+
+            stopwatch.Stop();
+
+            // Assert
+            Assert.True(stopwatch.ElapsedMilliseconds < 5000, "La creación de 1000 tareas debería tomar menos de 5 segundos");
+
+        }
+    }
+}
